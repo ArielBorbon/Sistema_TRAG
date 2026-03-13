5bc56a99cacb542e9f1f11928de0215bcad9c13e
ArielBorbon <ariel.borbon252116@potros.itson.edu.mx>
2026-03-13 13:04:10 -0700

    Estructura Del proyecto
    POM proyect con todos los proyectos dentro que utilizaremos

---
+++ TRAG_POM/Negocio_TRAG/src/main/java/com/mycompany/negocio_trag/Negocio_TRAG.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.negocio_trag;
+
+/**
+ *
+ * @author PC Gamer
+ */
+public class Negocio_TRAG {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ TRAG_POM/Presentacion_TRAG/src/main/java/com/mycompany/presentacion_trag/Presentacion_TRAG.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.presentacion_trag;
+
+/**
+ *
+ * @author PC Gamer
+ */
+public class Presentacion_TRAG {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
