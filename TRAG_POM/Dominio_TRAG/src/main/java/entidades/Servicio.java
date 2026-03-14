ddbbcbe6d617602707599f0a68d473bfe747eaad
ArielBorbon <ariel.borbon252116@potros.itson.edu.mx>
2026-03-14 05:44:34 -0700

    Entidades Servicio e InsumoServicio

---
+++ TRAG_POM/Dominio_TRAG/src/main/java/entidades/Servicio.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author PC Gamer
+ */
+@Entity
+@Table(name = "Servicio")
+public class Servicio implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 150)
+    private String nombre;
+
+    @Column(columnDefinition = "TEXT")
+    private String descripcion;
+
+    @Column(precision = 10, scale = 2)
+    private BigDecimal precioManoObraSugerido;
+}
