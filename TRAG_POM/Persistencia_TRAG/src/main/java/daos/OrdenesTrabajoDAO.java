b53b87116656c8b20ba0f7846b492236cc2e274c
manuel-romo <manuel.romo253080@potros.itson.edu.mx>
2026-03-17 20:04:20 -0700

    Corrección OrdenesTrabajoDAO

--- TRAG_POM/Persistencia_TRAG/src/main/java/daos/OrdenesTrabajoDAO.java
+++ TRAG_POM/Persistencia_TRAG/src/main/java/daos/OrdenesTrabajoDAO.java
@@ -1,8 +1,12 @@
 
 package daos;
 
+import conexion.Conexion;
+import entidades.Automovil;
+import entidades.Cotizacion;
 import entidades.OrdenTrabajo;
 import interfaces.IOrdenesTrabajoDAO;
+import javax.persistence.EntityManager;
 
 /**
  *
@@ -10,14 +14,49 @@ import interfaces.IOrdenesTrabajoDAO;
  */
 public class OrdenesTrabajoDAO implements IOrdenesTrabajoDAO{
 
+    private final String MENSAJE_ERROR_AGREGAR = "Error al agregar la orden de trabajo.";
+    private final String MENSAJE_ERROR_CONSULTA = "Error al consultar la orden de trabajo.";
+    
     @Override
     public OrdenTrabajo crearOrdenTrabajo(OrdenTrabajo ordenTrabajo) {
-        return null;
+        
+        EntityManager em = Conexion.crearConexion();
+        try {
+            em.getTransaction().begin();
+            
+            // Obtención de referencias
+            Automovil referenciaAutomovil = em.getReference(Automovil.class, ordenTrabajo.getAutomovil().getId());
+            Cotizacion referenciaCotizacion = em.getReference(Cotizacion.class, ordenTrabajo.getCotizacion().getId());
+            
+            ordenTrabajo.setAutomovil(referenciaAutomovil);
+            ordenTrabajo.setCotizacion(referenciaCotizacion);
+            
+            em.persist(ordenTrabajo);
+            
+            em.getTransaction().commit();
+            return ordenTrabajo;
+            
+        } catch (Exception e) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            throw new RuntimeException(MENSAJE_ERROR_AGREGAR, e);
+        } finally {
+            em.close();
+        }
     }
 
     @Override
     public OrdenTrabajo obtenerOrdenTrabajo(Long idOrdenTrabajo) {
-        return null;
+        
+        EntityManager em = Conexion.crearConexion();
+        try {
+            return em.find(OrdenTrabajo.class, idOrdenTrabajo);
+        } catch (Exception e) {
+            throw new RuntimeException(MENSAJE_ERROR_CONSULTA, e);
+        } finally {
+            em.close();
+        }
     }
     
 }
